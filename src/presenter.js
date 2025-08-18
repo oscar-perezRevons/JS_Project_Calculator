3c18db4047d91e47d90434cb4de202edc8032e85
oscar-perezRevons <oscarsamuelperezcallizaya@gmail.com>
2025-08-17 22:38:41 -0400

    Add multiply functionality

--- src/presenter.js
+++ src/presenter.js
@@ -1,8 +1,9 @@
 import sumar from "./sumador";
+import multiplicar from "./multiplicador";
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
+const form = document.querySelector("#form");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
@@ -11,5 +12,11 @@ form.addEventListener("submit", (event) => {
   const firstNumber = Number.parseInt(first.value);
   const secondNumber = Number.parseInt(second.value);
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  const accion = event.submitter.value;
+
+  if (accion === "Sumar"){
+    div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  } else if (accion === "Multiplicar"){
+    div.innerHTML = "<p>" + multiplicar(firstNumber, secondNumber) + "</p>";
+  }
 });
